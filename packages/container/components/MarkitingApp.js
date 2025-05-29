4fa0f85a53f0e9988f5ab255689a31e8448d3593
mennabassam1 <menna.bassam@milango.app>
2025-05-29 18:28:11 +0300

    test

--- packages/container/components/MarkitingApp.js
+++ packages/container/components/MarkitingApp.js
@@ -1,5 +1,5 @@
 import React, { useRef,useEffect } from "react";
-import { mount } from "marketing/MarketingApp";
+import { mount } from "marketing/bootstrap";
 
 export default () => {
     const ref = useRef(null);
