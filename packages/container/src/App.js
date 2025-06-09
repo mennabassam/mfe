daaf5a713bf93a8092b34d6add4da7da5a3265aa
mennabassam1 <menna.bassam@milango.app>
2025-06-09 17:15:06 +0300

    fix typo

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import MarkitingApp from "../components/MarkitingApp";
 import Header from "../components/Header";
-import {stylesProvider, createGenerateClassName} from "@material-ui/core";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core";
 const generateClassName = createGenerateClassName({
     productionPrefix: "ca", 
   });
 
 export default ()=>{
     return <>
-    <stylesProvider generateClassName={generateClassName}>
+    <StylesProvider generateClassName={generateClassName}>
     <Header />
        <hr /> 
     <MarkitingApp />
-    </stylesProvider>
+    </StylesProvider>
     </>
 }
