b2088cb408f984d9aa120e3132b4b336f736e4a5
mennabassam1 <menna.bassam@milango.app>
2025-06-09 17:09:56 +0300

    fix genrating random class names issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import MarkitingApp from "../components/MarkitingApp";
 import Header from "../components/Header";
+import {stylesProvider, createGenerateClassName} from "@material-ui/core";
+const generateClassName = createGenerateClassName({
+    productionPrefix: "ca", 
+  });
 
 export default ()=>{
     return <>
+    <stylesProvider generateClassName={generateClassName}>
     <Header />
        <hr /> 
     <MarkitingApp />
+    </stylesProvider>
     </>
 }
